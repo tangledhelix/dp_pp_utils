60c1066e848d59536582ec8cdb596bbdbfc1d520
Dan Lowe <dan@tangledhelix.com>
2016-08-10 09:49:43 -0400

    Make UTF8 preservation hack a DP proofer note

--- make_project.py
+++ make_project.py
@@ -58,7 +58,7 @@ class MakeProject():
             contents = file.read()
 
         with open(output_file, 'w', encoding='utf-8') as file:
-            file.write('<!-- UTF8 preservation hack: Phœnix -->\n')
+            file.write('[** UTF8 preservation hack: Phœnix]\n')
             file.write(contents)
 
 
